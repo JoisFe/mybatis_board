460450f4fafe11be2e65bc053e581c4f4a2e23b3
ComSun <computerhermit96@gmail.com>
2022-05-19 13:14:01 +0900

    Feat: implement DefaultPostService

--- board/src/main/java/com/nhnacademy/jdbc/board/post/service/impl/DafaultPostService.java
+++ board/src/main/java/com/nhnacademy/jdbc/board/post/service/impl/DafaultPostService.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.jdbc.board.post.service.impl;
 
+import com.nhnacademy.jdbc.board.exception.PostNotFoundException;
 import com.nhnacademy.jdbc.board.post.domain.Post;
 import com.nhnacademy.jdbc.board.post.mapper.PostMapper;
 import com.nhnacademy.jdbc.board.post.service.PostService;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +19,8 @@ public class DafaultPostService implements PostService {
 
     @Override
     public Optional<Post> getPostByPostNum(Long postNum) {
-        return postMapper.selectPostByPostNum(postNum);
+        return Optional.ofNullable(postMapper.selectPostByPostNum(postNum)
+            .orElseThrow(() -> new PostNotFoundException("해당 번호의 게시글이 존재하지 않습니다.")));
     }
 
     @Override
@@ -27,16 +30,16 @@ public class DafaultPostService implements PostService {
 
     @Override
     public void insertPost(Post post) {
-
+        postMapper.insertPost(post);
     }
 
     @Override
     public void modifyPost(Long postNum, String postTitle, String postContent) {
-
+        postMapper.updatePostByPostNum(postNum, postTitle, postContent);
     }
 
     @Override
     public void deletePost(Long postNum) {
-
+        postMapper.deletePostByPostNum(postNum);
     }
 }
